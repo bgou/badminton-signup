57e48251b79098e1fdeef1669bed911278104e04
Bruce Gou <brucegou@amazon.com>
2018-11-15 16:17:42 -0800

    Refined process

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import puppeteer from "puppeteer";
 import moment from "moment";
 
 export const dateFormat = "MM/D/YYYY h:mm:ss a";
+const register2Url = "http://www.seattlebadmintonclub.com/Register2.aspx";
 
 export class AutomaticRegister {
   constructor() {
@@ -12,13 +13,18 @@ export class AutomaticRegister {
     this.browser = await puppeteer.launch();
     this.page = await this.browser.newPage();
 
-    this.login();
-    this.chooseDate();
-    this.choosePartner();
-    this.submitRegistration();
+    try {
+      await this.login();
+      await this.chooseDate();
+      await this.choosePartner();
+      await this.submitRegistration();
+    } catch (ex) {
+      console.error(ex);
+    }
   }
 
   async login() {
+    console.log("Opening login page");
     await this.page.goto("http://www.seattlebadmintonclub.com/Security.aspx");
 
     const leagueSelector =
@@ -40,11 +46,14 @@ export class AutomaticRegister {
       "#ctl00_bodyContentPlaceHolder_Login1_Password",
       "$badminton"
     );
+
+    console.log("Logging in");
     await this.page.click("#ctl00_bodyContentPlaceHolder_Login1_LoginButton");
     await this.page.waitForNavigation();
   }
 
   async chooseDate() {
+    console.log('Choosing date');
     const playDateSelector = "#ctl00_bodyContentPlaceHolder_ddlistPlayDate";
     await this.page.waitForSelector(playDateSelector);
     const playDateSelectElem = await this.page.$(playDateSelector);
@@ -64,18 +73,19 @@ export class AutomaticRegister {
       nextTuesOpt.value
     );
     await this.page.waitForResponse(
-      "http://www.seattlebadmintonclub.com/Register2.aspx"
+      register2Url
     );
+    console.log(`Selected ${nextTuesOpt.text}`)
   }
 
   async choosePartner() {
-    console.log("choosing partner");
+    console.log("Choosing partner");
     await this.page.select(
       "#ctl00_bodyContentPlaceHolder_listUnselected",
       "tom nguyen 141"
     );
     await this.page.waitForResponse(
-      "http://www.seattlebadmintonclub.com/Register2.aspx"
+      register2Url
     );
   }
 
@@ -84,7 +94,7 @@ export class AutomaticRegister {
     const registerButtonSelector = "#ctl00_bodyContentPlaceHolder_registerTB";
     await this.page.click(registerButtonSelector);
     await this.page.waitForResponse(
-      "http://www.seattlebadmintonclub.com/Register2.aspx"
+      register2Url
     );
 
     await this.page.screenshot({ path: "example.png" });
