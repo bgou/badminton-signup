460512104ad7377234797363ea27a63093d37cf7
Bruce Gou <brucegou@amazon.com>
2018-12-04 16:37:22 -0800

    changed logging debug level

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker run -d \
   --name badminton \
   -e LOG_LEVEL=debug \
   -v $HOME/badminton-signup/log:/usr/src/app/log \
-  badminton
+  052852330140.dkr.ecr.us-west-2.amazonaws.com/badminton
